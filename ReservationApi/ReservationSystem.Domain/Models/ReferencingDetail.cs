feeaa1b2ac1b3d50e33ac7431b577326c99f7c1a
Amjad Ismail <amjad@jaystravels.co.uk>
2025-02-11 09:36:50 +0500

    baggage details

---
+++ ReservationApi/ReservationSystem.Domain/Models/ReferencingDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservationSystem.Domain.Models
+{
+    public class ReferencingDetail
+    {
+        public int? refNumber { get; set; }
+        public string? refQualifier { get; set; }
+    }
+}
